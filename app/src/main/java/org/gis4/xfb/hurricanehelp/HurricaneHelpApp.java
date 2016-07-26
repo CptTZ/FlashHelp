da7d8d13605952dd80e6cb9b6fa00780c1827b07
zhangchi <zcqzcq@outlook.com>
2016-07-26 20:08:57 +0800

    push test
    基佬宜

--- app/src/main/java/org/gis4/xfb/hurricanehelp/HurricaneHelpApp.java
+++ app/src/main/java/org/gis4/xfb/hurricanehelp/HurricaneHelpApp.java
@@ -14,6 +14,8 @@ public class HurricaneHelpApp extends Application
         AVOSCloud.initialize(this, "LGhLGudXgoF9oTXHTr1an03Y-gzGzoHsz", "z4Dou3Q1LNlmG176tECNBxQh");
         AVAnalytics.setAnalyticsEnabled(true);
         AVAnalytics.enableCrashReport(this, false);
+
+        //gittest 张宜弛大傻逼
     }
 
 }
